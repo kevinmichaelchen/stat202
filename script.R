dc2950979bdece736a2d6eb1970b3fd78bff91c5
kevinchen93 <meejathoughts@gmail.com>
2014-05-07 11:18:48 -0400

    added Agricultural models

--- script.R
+++ script.R
@@ -1,125 +1,64 @@
-#' Checks for Not A Number (NaN) values.
-isAllNumbers <- function(dataFrame) {
-  isNumber.allColumns = rep(TRUE, ncol(dataFrame))
-  for (z in 1:ncol(dataFrame)) {
-    for (l in 1:nrow(dataFrame)) {
-      if (is.nan(dataFrame[l,z]) == TRUE) {
-        isNumber.allColumns[z] = FALSE
-        print(paste("Column", z, "contains Not A Number (NaN) value(s)..."))
-        break
-      }
-    }
+# Load the data frame
+df <- read.csv(file.choose(), header = TRUE, check.names = FALSE)
+
+# Our response
+log.GDP.per.capita <- log(df$`GDP per capita (current US$)`)
+
+
+# AGRICULTURAL INDICATORS
+rural.water <- df$`Improved water source, rural (% of rural population with access)`
+rural.poor <- df$`Poverty headcount ratio at urban poverty line (% of urban population)`
+food.production <- df$`Food production index (2004-2006 = 100)`
+arable.land <- df$`Arable land (% of land area)`
+rural.population.percent <- df$`Rural population (% of total population)`
+
+predictors <- c(rural.water,rural.poor,food.production,arable.land,rural.population.percent)
+names <- c("Rural Access to Water","Rural Poverty (%)","Food Production","Arable Land","Rural Population (% of Total)")
+
+
+# Regressions
+summary(lm(log.GDP.per.capita ~ rural.water + rural.poor + food.production + arable.land + rural.population.percent))
+summary(lm(log.GDP.per.capita ~ rural.water + rural.poor + rural.population.percent))
+summary(lm(log.GDP.per.capita ~ rural.poor + rural.population.percent))
+summary(lm(log.GDP.per.capita ~ rural.water + rural.population.percent))
+
+for (i in 1:length(predictors)) {
+  if (names(predictors)[i] == "Food.production") {
+    plot(predictors[[i]], log.GDP.per.capita, xlab = names[i])
+    abline(v = 100)
   }
-  return(length(which(isNumber.allColumns == FALSE)) <= 0)
-}
-
-#' Checks for log-ability.
-#' A number can be logged if it is a non-zero positive.
-checkAllLoggable <- function(dataFrame) {
-  logs <- rep(TRUE, ncol(dataFrame))
-  for (z in 1:ncol(dataFrame)) {
-    for (l in 1:nrow(dataFrame)) {
-      cell = dataFrame[l,z]
-      if (is.na(cell) || class(cell) == 'logical') {next}
-      if (as.numeric(cell) <= 0) {        
-        print(paste("Column", z, "cannot be logged due to a", cell))
-        logs[z] = FALSE
-        break
-      }
-    }
+  else {
+    plot(predictors[[i]], log.GDP.per.capita, xlab = names[i])
+    abline(lm(log.GDP.per.capita ~ predictors[[i]]))
   }
-  return(logs)
-}
-
-#' Returns two lists of multiple regressions.
-#' @param dataFrame The data frame.
-#' @param dfName The name of the data frame.
-#' @param response The response will not appear in the result, since you can't regress an indicator on itself.
-#' @return The first list consists of log multiple regressions, the second of non-log regressions.
-getRegressionLists <- function(dataFrame, dfName, response) {
-  variables = names(dataFrame)
-  colsign = checkAllLoggable(dataFrame)
-  print("got out of check all logable")
-  list.lm.log = list()
-  list.lm = list()
-  names = names(dataFrame)
-  for (i in 2:ncol(dataFrame)) {
-    indicator = names[i]
-    if (indicator == response) {
-      next
-    }                   
-    print(paste("Checking indicator:", indicator))
-    if (colsign[i]) {
-      list.lm.log[[i]] = eval(parse(text = paste0(
-        "lm(log(`", response,"`) ~log(`", variables[i], "`), data = ", dfName, ", na.action = na.exclude)"
-      )))
-    }
-    else {
-      list.lm[[i]] = eval(parse(text = paste0(
-        "lm(log(`", response, "`) ~ (`", variables[i], "`), data = ", dfName, ", na.action = na.exclude)"
-      )))
-    }
-  }
-  length(list.lm.log)
-  length(list.lm)
-  return(list("logs"=list.lm.log, "noLogs"=list.lm))
-}
-
-#' Returns a list of the best covariates.
-#' A covariate x is deemed good if cor(x,y) > cor.threshold.
-#' @param lm.list A list of multiple regression objects
-#' @param cor.threshold Defines which covariates are deemed "good"
-#' @param names The names of variables in lm.list
-listBestCovariates <- function(lm.list, cor.threshold, names) {
-  summaries <- lapply(lm.list, summary)
-  cors <- unlist(lapply(summaries,"[","r.squared"))
-  cors.adj <- unlist(lapply(summaries,"[","adj.r.squared"))
-  names(cors.adj) <- names
-  cors.adj.num <- as.numeric(cors.adj)
-  cors.adj.num.culled <- (cors.adj.num > cor.threshold)
-  covariates <- grep("TRUE", cors.adj.num.culled)
-  return(covariates)
 }
 
 
 
-# LOAD THE DATA FRAME
-df <- read.csv(file.choose(), header = TRUE, check.names = TRUE)
-variables <- names(df)
-names(df) <- lapply(variables, function(x) {
-  
-  # if first two letters are 'X.', lop off 'X'
-  if (substr(x, 1, 2) == 'X.') {
-    x <- substr(x,2,nchar(x))    
-  }
-  
-  # split based on dots
-  chars <- unlist(strsplit(x, "[.]"))
-  
-  # remove empty tokens (i.e., remove double/triple dots)
-  new.chars <- Filter(function(x) x != "", chars)
-  
-  # join the characters back into a string
-  return(paste(new.chars, collapse = '.'))
-})
+# URBAN INDICATORS
+motor.vehicles.per.1000 <- df$`Motor vehicles (per 1,000 people)`
+urban.water <- df$`Improved water source, urban (% of urban population with access)`
+urban.sanitation <- df$`Improved sanitation facilities, urban (% of urban population with access)`
 
-# Fix one duplicate
-names(df)[1074] <- "Secondary.education.teachers.female.percentage"
+urban.primacy <- df$`Population in the largest city (% of urban population)`
+urban.population <- df$`Urban population (% of total)`
 
-response <- "GDP.per.capita.constant.2005.US"
-p1 <- "General.government.final.consumption.expenditure.of.GDP"
-f1 <- paste(response, "~", p1)
-summary(lm(as.formula(f1), data = df))
+#' mean shortfall from poverty line as a percentage
+#' of national urban poverty line. If the national
+#' urban poverty line is $20,000, and there are two people
+#' in the city -- a rich man making more than 20k, with 0 
+#' shortfall, and a poor man making 15k with 5k shortfall
+#' -- then the average shortfall, 2.5k, yields a 
+#' 12.5% poverty gap. Measures not just incidence, but also
+#' depth of poverty.
+urban.poverty.gap <- df$`Poverty gap at urban poverty line (%)`
 
-agriculture <- c(46:63, )
+# Incidence of poverty as percentage of urban population
+urban.poverty.percentage <- df$`Poverty headcount ratio at urban poverty line (% of urban population)`
 
-if (isAllNumbers(ourFrame)) {
-  print("No Columns Contain NaNs")
-}
+price.of.gas <- df$`Pump price for gasoline (US$ per liter)`
+price.of.diesel <- df$`Pump price for diesel fuel (US$ per liter)`
+road.sector.energy <- df$`Road sector energy consumption (% of total energy consumption)`
 
-reg.lists <- getRegressionLists(ourFrame, "ourFrame", "GDP (current US$)")
-list.lm.log <- reg.lists$logs
-list.lm <- reg.lists$noLogs
 
-covariates.logged <- listBestCovariates(list.lm.log, 0.5, variables)
-covariates <- listBestCovariates(list.lm, 0.5, variables)
+ 

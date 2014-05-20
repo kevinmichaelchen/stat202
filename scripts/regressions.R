449b924dc981b6a3f6b49b6062d8e808f84c6a3b
kevinchen93 <meejathoughts@gmail.com>
2014-05-20 16:49:31 -0400

    added jpeg reconstruction to regressions script

--- scripts/regressions.R
+++ scripts/regressions.R
@@ -65,7 +65,7 @@ pred <- c(motor.vehicles.per.1000,
           price.of.diesel,
           road.sector.energy,
           urban.water
-          )
+)
 
 for (i in 1:length(pred)) {
   p = pred[i]
@@ -134,9 +134,9 @@ write.conditions <- function(df.names, titles, path){
   jpeg(paste0(path, substitute(titles.cli), "_logged1.jpeg"), width = 1000*n, height = 200*n)
   
   # 2 rows, one for residuals vs fitted, one for qqline
-  par(mfrow = c(1,n),omd=c(4,4.5,2,1))
+  par(mfrow = c(1,n), omd = c(4,4.5,2,1))
   
-  for(i in 1:n){
+  for(i in 1:n) {
     # j is 1 because the resid vs fitted is the 1st plot given by plot(lm)
     j <- 1  
     my.predictor = eval(parse(text = paste0("log(df$`",df.names[i],"`)")))
@@ -364,5 +364,483 @@ summary(lm(log.GDP.per.capita ~ log.life.expect + log.refugees))
 
 
 summary(lm(log.GDP.per.capita ~ log.high.tech * log.research + log.high.tech * log.journals + log.research * log.journals + rural.water + rural.population.percent + urban.sanitation + paved.roads + electricity.consumption + log.emissions.co2.kt + log.life.expect + log.refugees))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#####################################
+#####################################
+#####################################
+
+IMG_PATH = "~/Desktop/STAT202/Final/writeups/images/"
+makePDF <- function(s) paste0(IMG_PATH, s, ".pdf")
+makePNG <- function(s) paste0(IMG_PATH, s, ".png")
+SCALE = 500
+
+nrow = 1
+ncol = 2
+png(filename = makePNG("urban_model_scatter_pop_water"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(x = urban.population,
+     xlab = "Urban Population (% of Total)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ urban.population)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = urban.water,
+     xlab = "% Urban Access to Clean Water",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ urban.water)),
+       lwd = 3.5,
+       col = "firebrick")
+g <- dev.off()
+
+
+
+
+
+nrow = 1
+ncol = 1
+png(filename = makePNG("urban_model_scatter_san"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(x = urban.sanitation,
+     xlab = "% of Urban Population with Sanitation Access",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ urban.sanitation)),
+       lwd = 3.5,
+       col = "firebrick")
+g <- dev.off()
+
+
+
+
+nrow = 1
+ncol = 2
+m <- lm(log.GDP.per.capita ~ urban.sanitation + urban.population)
+summary(m)
+png(filename = makePNG("urban_model_conditions"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(m, which=c(1,2))
+g <- dev.off()
+
+
+
+
+co2 <- df$`CO2 emissions (kt)`
+methane <- df$`Methane emissions (kt of CO2 equivalent)`
+nitrous <- df$`Nitrous oxide emissions (thousand metric tons of CO2 equivalent)`
+electricity.consumption <- df$`Electric power consumption (kWh per capita)`
+paved.roads <- df$`Roads, paved (% of total roads)`
+
+log.co2 <- log(co2 + 0.01)
+log.methane <- log(methane + 0.01)
+log.nitrous <- log(nitrous + 0.01)
+log.electricity.consumption <- log(electricity.consumption + 0.01)
+log.paved.roads <- log(paved.roads + 0.01)
+
+
+
+
+nrow = 2
+ncol = 3
+png(filename = makePNG("climate_model_scatter_pollution"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(x = co2,
+     xlab = "CO2 Emissions (kt)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ co2)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = methane,
+     xlab = "Methane Emissions (kt)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ methane)),
+       lwd = 3.5,
+       col = "firebrick")
+
+
+plot(x = nitrous,
+     xlab = "Nitrous Emissions (kt)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ nitrous)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.co2,
+     xlab = "log CO2 Emissions (kt)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.co2)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.methane,
+     xlab = "log Methane Emissions (kt)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.methane)),
+       lwd = 3.5,
+       col = "firebrick")
+
+
+plot(x = log.nitrous,
+     xlab = "log Nitrous Emissions (kt)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.nitrous)),
+       lwd = 3.5,
+       col = "firebrick")
+g <- dev.off()
+
+
+
+
+
+
+nrow = 2
+ncol = 2
+png(filename = makePNG("climate_model_scatter_elec_roads"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(x = electricity.consumption,
+     xlab = "Electricity Consumption",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ electricity.consumption)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = paved.roads,
+     xlab = "Paved Roads (% of total)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ paved.roads)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.electricity.consumption,
+     xlab = "log Electricity Consumption",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.electricity.consumption)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.paved.roads,
+     xlab = "log Paved Roads (% of total)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.paved.roads)),
+       lwd = 3.5,
+       col = "firebrick")
+g <- dev.off()
+
+
+
+m1 <- lm(log.GDP.per.capita ~ log.electricity.consumption*log.co2)
+summary(m1)
+m2 <- lm(log.GDP.per.capita ~ log.electricity.consumption:log.co2 + log.co2)
+summary(m2)
+
+nrow = 1
+ncol = 2
+png(filename = makePNG("climate_model_conditions"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(m2, which=c(1,2))
+g <- dev.off()
+
+
+
+
+
+
+
+
+high.tech <- df$`High-technology exports (% of manufactured exports)`
+r.n.d <- df$`Research and development expenditure (% of GDP)`
+journals <- df$`Scientific and technical journal articles`
+log.high.tech <- log(high.tech + 0.01)
+log.research <- log(r.n.d + 0.01)
+log.journals <- log(journals + 0.01)
+
+
+
+nrow = 2
+ncol = 3
+png(filename = makePNG("science_model_scatter"), width = SCALE * ncol, height = SCALE * nrow)
+par(mfrow=c(nrow,ncol))
+plot(x = high.tech,
+     xlab = "High-tech exports (% of manufactured exports)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ high.tech)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = r.n.d,
+     xlab = "R&D Expenditure (% of GDP)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ r.n.d)),
+       lwd = 3.5,
+       col = "firebrick")
+
+
+plot(x = journals,
+     xlab = "Science Journals",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ journals)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.high.tech,
+     xlab = "log High-tech exports (% of manufactured exports)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.high.tech)),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.research,
+     xlab = "log R&D Expenditure (% of GDP)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.research)),
+       lwd = 3.5,
+       col = "firebrick")
+
+
+plot(x = log.journals,
+     xlab = "log Science Journals",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(summary(lm(log.GDP.per.capita ~ log.journals)),
+       lwd = 3.5,
+       col = "firebrick")
+g <- dev.off()
+
+
+
+
+
+
+m <- lm(log.GDP.per.capita ~ log.high.tech * log.journals)
+summary(m)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+child.labor <- df$`Children in employment, total (% of children ages 7-14)`
+life.expect <- df$`Life expectancy at birth, male (years)`
+refugees <- df$`Refugee population by country or territory of origin`
+literacy <- df$`Literacy rate, adult total (% of people ages 15 and above)`
+
+log.child.labor <- log(child.labor + 0.01)
+log.life.expect <- log(life.expect + 0.01)
+log.refugees <- log(refugees + 0.01)
+log.literacy <- log(literacy + 0.01)
+
+
+
+
+
+m <- lm(log.GDP.per.capita ~ child.labor + life.expect + refugees + literacy)
+summary(m)
+m <- lm(log.GDP.per.capita ~ life.expect + refugees)
+summary(m)
+par(mfrow=c(1,2))
+plot(m, which=c(1,2))
+
+
+
+
+par(mfrow=c(2,4))
+plot(x = child.labor,
+     xlab = "Child Employment (% of children ages 7-14)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ child.labor),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = life.expect,
+     xlab = "Life expectancy at birth, male (years)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ life.expect),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = refugees,
+     xlab = "Refugee Population",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ refugees),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = literacy,
+     xlab = "Literacy Rate (adult total)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ literacy),
+       lwd = 3.5,
+       col = "firebrick")
+
+
+
+
+plot(x = log.child.labor,
+     xlab = "log Child Employment (% of chlidren ages 7-14)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ log.child.labor),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.life.expect,
+     xlab = "log Life expectancy at birth, male (years)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ log.life.expect),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.refugees,
+     xlab = "log Refugee Population",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ log.refugees),
+       lwd = 3.5,
+       col = "firebrick")
+
+plot(x = log.literacy,
+     xlab = "log Literacy Rate (adult total)",
+     y = log.GDP.per.capita,
+     ylab = "log(GDP per capita)",
+     pch = 17, #dot point
+     cex = 1.2, #dot size
+     col = "darkblue")
+abline(lm(log.GDP.per.capita ~ log.literacy),
+       lwd = 3.5,
+       col = "firebrick")
+
+
+
+m <- lm(log.GDP.per.capita ~ log.life.expect + log.refugees)
+summary(m)
+par(mfrow=c(1,2))
+plot(m, which=c(1,2))
+
+
+
 
 

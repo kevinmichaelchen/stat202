912a82981c7f07b68c709b1a29553b87d16e7e73
kevinchen93 <meejathoughts@gmail.com>
2014-05-20 19:40:32 -0400

    made variable for line color and point color

--- scripts/images.R
+++ scripts/images.R
@@ -14,7 +14,8 @@ PLOT_WIDTH = 1000
 PCH = 17
 CEX = 3
 LWD = 8
-
+POINT_COLOR = "darkblue"
+LINE_COLOR = "firebrick"
 
 # HEAT MAP
 #require(rworldmap)
@@ -72,11 +73,11 @@ nrow = 1
 ncol = 2
 png(filename = makePNG("urban_model_scatter_pop_water"), width = PLOT_WIDTH * ncol, height = PLOT_WIDTH * nrow)
 par(mfrow=c(nrow,ncol))
-plot(x = urban.population, xlab = "Urban Population (% of Total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ urban.population)), lwd = LWD, col = "firebrick")
+plot(x = urban.population, xlab = "Urban Population (% of Total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ urban.population)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = urban.water, xlab = "% Urban Access to Clean Water", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ urban.water)), lwd = LWD, col = "firebrick")
+plot(x = urban.water, xlab = "% Urban Access to Clean Water", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ urban.water)), lwd = LWD, col = LINE_COLOR)
 g <- dev.off()
 
 
@@ -86,8 +87,8 @@ nrow = 1
 ncol = 1
 png(filename = makePNG("urban_model_scatter_san"), width = PLOT_WIDTH * ncol, height = PLOT_WIDTH * nrow)
 par(mfrow=c(nrow,ncol))
-plot(x = urban.sanitation, xlab = "% of Urban Population with Sanitation Access", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ urban.sanitation)), lwd = LWD, col = "firebrick")
+plot(x = urban.sanitation, xlab = "% of Urban Population with Sanitation Access", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ urban.sanitation)), lwd = LWD, col = LINE_COLOR)
 g <- dev.off()
 
 
@@ -116,23 +117,23 @@ nrow = 2
 ncol = 3
 png(filename = makePNG("climate_model_scatter_pollution"), width = PLOT_WIDTH * ncol, height = PLOT_WIDTH * nrow)
 par(mfrow=c(nrow,ncol))
-plot(x = co2, xlab = "CO2 Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ co2)), lwd = LWD, col = "firebrick")
+plot(x = co2, xlab = "CO2 Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ co2)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = methane, xlab = "Methane Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ methane)), lwd = LWD, col = "firebrick")
+plot(x = methane, xlab = "Methane Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ methane)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = nitrous, xlab = "Nitrous Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ nitrous)), lwd = LWD, col = "firebrick")
+plot(x = nitrous, xlab = "Nitrous Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ nitrous)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.co2, xlab = "log CO2 Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.co2)), lwd = LWD, col = "firebrick")
+plot(x = log.co2, xlab = "log CO2 Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.co2)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.methane, xlab = "log Methane Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.methane)), lwd = LWD, col = "firebrick")
+plot(x = log.methane, xlab = "log Methane Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.methane)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.nitrous, xlab = "log Nitrous Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.nitrous)), lwd = LWD, col = "firebrick")
+plot(x = log.nitrous, xlab = "log Nitrous Emissions (kt)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.nitrous)), lwd = LWD, col = LINE_COLOR)
 g <- dev.off()
 
 
@@ -142,17 +143,17 @@ nrow = 2
 ncol = 2
 png(filename = makePNG("climate_model_scatter_elec_roads"), width = PLOT_WIDTH * ncol, height = PLOT_WIDTH * nrow)
 par(mfrow=c(nrow,ncol))
-plot(x = electricity.consumption, xlab = "Electricity Consumption", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ electricity.consumption)), lwd = LWD, col = "firebrick")
+plot(x = electricity.consumption, xlab = "Electricity Consumption", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ electricity.consumption)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = paved.roads, xlab = "Paved Roads (% of total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ paved.roads)), lwd = LWD, col = "firebrick")
+plot(x = paved.roads, xlab = "Paved Roads (% of total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ paved.roads)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.electricity.consumption, xlab = "log Electricity Consumption", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.electricity.consumption)), lwd = LWD, col = "firebrick")
+plot(x = log.electricity.consumption, xlab = "log Electricity Consumption", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.electricity.consumption)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.paved.roads, xlab = "log Paved Roads (% of total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.paved.roads)), lwd = LWD, col = "firebrick")
+plot(x = log.paved.roads, xlab = "log Paved Roads (% of total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.paved.roads)), lwd = LWD, col = LINE_COLOR)
 g <- dev.off()
 
 
@@ -184,23 +185,23 @@ nrow = 2
 ncol = 3
 png(filename = makePNG("science_model_scatter"), width = PLOT_WIDTH * ncol, height = PLOT_WIDTH * nrow)
 par(mfrow=c(nrow,ncol))
-plot(x = high.tech, xlab = "High-tech exports (% of manufactured exports)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ high.tech)), lwd = LWD, col = "firebrick")
+plot(x = high.tech, xlab = "High-tech exports (% of manufactured exports)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ high.tech)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = research, xlab = "R&D Expenditure (% of GDP)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ research)), lwd = LWD, col = "firebrick")
+plot(x = research, xlab = "R&D Expenditure (% of GDP)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ research)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = journals, xlab = "Science Journals", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ journals)), lwd = LWD, col = "firebrick")
+plot(x = journals, xlab = "Science Journals", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ journals)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.high.tech, xlab = "log High-tech exports (% of manufactured exports)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.high.tech)), lwd = LWD, col = "firebrick")
+plot(x = log.high.tech, xlab = "log High-tech exports (% of manufactured exports)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.high.tech)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.research, xlab = "log R&D Expenditure (% of GDP)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.research)), lwd = LWD, col = "firebrick")
+plot(x = log.research, xlab = "log R&D Expenditure (% of GDP)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.research)), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.journals, xlab = "log Science Journals", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(summary(lm(log.GDP.per.capita ~ log.journals)), lwd = LWD, col = "firebrick")
+plot(x = log.journals, xlab = "log Science Journals", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(summary(lm(log.GDP.per.capita ~ log.journals)), lwd = LWD, col = LINE_COLOR)
 g <- dev.off()
 
 
@@ -245,29 +246,29 @@ nrow = 2
 ncol = 4
 png(filename = makePNG("social_model_scatter"), width = PLOT_WIDTH * ncol, height = PLOT_WIDTH * nrow)
 par(mfrow=c(nrow,ncol))
-plot(x = child.labor, xlab = "Child Employment (% of children ages 7-14)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ child.labor), lwd = LWD, col = "firebrick")
+plot(x = child.labor, xlab = "Child Employment (% of children ages 7-14)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ child.labor), lwd = LWD, col = LINE_COLOR)
 
-plot(x = life.expect, xlab = "Life expectancy at birth, male (years)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ life.expect), lwd = LWD, col = "firebrick")
+plot(x = life.expect, xlab = "Life expectancy at birth, male (years)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ life.expect), lwd = LWD, col = LINE_COLOR)
 
-plot(x = refugees, xlab = "Refugee Population", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ refugees), lwd = LWD, col = "firebrick")
+plot(x = refugees, xlab = "Refugee Population", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ refugees), lwd = LWD, col = LINE_COLOR)
 
-plot(x = literacy, xlab = "Literacy Rate (adult total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ literacy), lwd = LWD, col = "firebrick")
+plot(x = literacy, xlab = "Literacy Rate (adult total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ literacy), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.child.labor, xlab = "log Child Employment (% of chlidren ages 7-14)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ log.child.labor), lwd = LWD, col = "firebrick")
+plot(x = log.child.labor, xlab = "log Child Employment (% of chlidren ages 7-14)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ log.child.labor), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.life.expect, xlab = "log Life expectancy at birth, male (years)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ log.life.expect), lwd = LWD, col = "firebrick")
+plot(x = log.life.expect, xlab = "log Life expectancy at birth, male (years)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ log.life.expect), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.refugees, xlab = "log Refugee Population", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ log.refugees), lwd = LWD, col = "firebrick")
+plot(x = log.refugees, xlab = "log Refugee Population", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ log.refugees), lwd = LWD, col = LINE_COLOR)
 
-plot(x = log.literacy, xlab = "log Literacy Rate (adult total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = "darkblue")
-abline(lm(log.GDP.per.capita ~ log.literacy), lwd = LWD, col = "firebrick")
+plot(x = log.literacy, xlab = "log Literacy Rate (adult total)", y = log.GDP.per.capita, ylab = "log(GDP per capita)", pch = PCH, cex = CEX, col = POINT_COLOR)
+abline(lm(log.GDP.per.capita ~ log.literacy), lwd = LWD, col = LINE_COLOR)
 g <- dev.off()
 
 
